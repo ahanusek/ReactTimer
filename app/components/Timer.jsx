a2167144c60b524b885be92b9f777ce0c8565987
hanys92 <hanusek.a@gmail.com>
2017-04-02 11:37:21 +0200

    Initial app commit

---
+++ app/components/Timer.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+
+
+var Timer = React.createClass({
+	render: function(){
+		return (
+			<div>
+				Timer component
+			</div>
+		);
+	}
+})
+
+module.exports = Timer;
