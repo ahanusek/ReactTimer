8720cabe5df4177e43797c8b882fb238d1de19ab
hanys92 <hanusek.a@gmail.com>
2017-04-03 23:15:15 +0200

    Add CountdownForm component

---
+++ app/components/CountdownForm.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+
+var CountdownForm = React.createClass({
+	onFormSubmit: function(e){
+		e.preventDefault();
+		var seconds = parseInt(this.refs.seconds.value, 10);
+		if(typeof seconds === "number" && seconds > 0){
+			this.props.onSetCountdown(seconds);
+		} else {
+			console.log('error value')
+		}
+		this.refs.seconds.value = "";
+	},
+	render: function(){
+		return (
+			<div className="countdown-form-container">
+				<form	onSubmit={this.onFormSubmit}>
+					<input type="text" ref="seconds" placeholder="Enter time in seconds"/>
+					<button className="button expanded" type="submit">Start</button>
+				</form>
+			</div>
+		)
+	}
+});
+
+module.exports = CountdownForm;
